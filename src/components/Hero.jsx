e45fa32108552df7cf48aa1cbee22d9fc61c846c
Indrajbir Singh <101132675+Indrajhere@users.noreply.github.com>
2024-07-23 00:32:32 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/kevinRushProfile.png";
+import profilePic from "../assets/indraj_vig.jpg";
 import { motion } from "framer-motion";
 const Hero = () => {
   const container = (delay) => ({
